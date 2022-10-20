d6f9126165fcdb8468f7e1051eafe3e9b138010d
Dovydas <dovydas@yoshke.org>
2022-10-20 16:07:42 +0100

    describe namespaces

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,15 @@ mod mime;
 mod rss;
 mod time;
 
+/// Namespace extension for Apple Podcasts.
+///
+/// Implementation details adapted from
+/// <https://help.apple.com/itc/podcasts_connect/#/itcb54353390>.
 pub mod itunes;
+
+/// Namespace extension for podcasting.
+///
+/// Implementation details adapted from <https://podcastindex.org/namespace/1.0>.
 pub mod podcast;
 
 pub use crate::basic::{Bool, Float, Integer, Number};
